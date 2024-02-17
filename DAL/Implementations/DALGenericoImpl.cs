6399acde8875599f04ce3a0a1cb7f0bb05361f5b
Kike <josellique@gmail.com>
2024-02-16 18:03:09 -0600

    DAL Base completado

---
+++ DAL/Implementations/DALGenericoImpl.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class DALGenericoImpl<TEntity> : IDALGenerico<TEntity> where TEntity : class
+    {
+
+        protected readonly CursosContext _Context;
+
+        public DALGenericoImpl(CursosContext context)
+        {
+            _Context = context;
+        }
+        public bool Add(TEntity entity)
+        {
+            try
+            {
+                _Context.Add(entity);
+                return true;
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;      
+            }
+        }
+
+        public TEntity Get(int id)
+        {
+            return _Context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _Context.Set<TEntity>().ToList();
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            try
+            {
+                _Context.Set<TEntity>().Attach(entity);
+                _Context.Set<TEntity>().Remove(entity);
+                return true;
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return true;
+            }
+        }
+    }
+}
---
+++ DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo : IUnidadDeTrabajo
+    {
+        private readonly CursosContext _context;
+
+        public UnidadDeTrabajo(CursosContext context)
+        {
+            _context = context;
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
